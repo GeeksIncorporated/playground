c7e06cc2b9d89d1f53ac13784d0f9d9df52db402
aliowka <aliowka@gmail.com>
2017-03-15 13:27:15 +0200

    Added: cooking_pizaa Addes: coins_change

---
+++ coins_change.py
@@ -0,0 +1,56 @@
+import time
+import sys
+sys.setrecursionlimit(100000)
+from collections import defaultdict
+
+table = defaultdict(lambda: defaultdict(int))
+
+
+def coins_change(n, coins):
+    if table[n][len(coins)]:
+        return table[n][len(coins)]
+
+    if n == 0:
+        return 1
+
+    if n < 0 or len(coins) == 0:
+        return 0
+
+    table[n][len(coins)] = coins_change(n, coins[:-1]) + coins_change(n - coins[-1], coins)
+    return table[n][len(coins)]
+
+
+def coins_change_bottom_up(n, coins):
+    table = defaultdict(lambda: defaultdict(int))
+    for i in range(1, n + 1):
+        table[i][1] = 1
+
+    for j in range(2, len(coins) + 1):
+        for i in range(1, n + 1):
+            if i < coins[j - 1]:
+                c = 0
+            elif i == coins[j - 1]:
+                c = 1
+            else:
+                c = table[i - coins[j - 1]][j]
+
+            table[i][j] = table[i][j - 1] + c
+
+    return table[i][j]
+
+
+n = 10000
+coins = [1, 2, 3, 5]
+
+st = time.time()
+r = coins_change(n, coins)
+t1 = time.time() - st
+print r, t1
+
+st = time.time()
+r = coins_change_bottom_up(n, coins)
+t2 = time.time() - st
+print r, t2
+
+print "Bottom up is faster x%.1f" % (float(t1)/t2)
+
