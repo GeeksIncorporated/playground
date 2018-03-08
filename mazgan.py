2b0d9a34e9346dff73249ece30b28ad9728e4531
aliowka-fornova <alexeyv@fornova.net>
2018-03-08 18:27:25 +0200

    balls and floors made for k balls stupid sort

---
+++ mazgan.py
@@ -0,0 +1,70 @@
+import sys
+import random
+from collections import defaultdict
+
+sys.setrecursionlimit(2024)
+
+# creating random array
+A = [random.randint(0, 100) for i in range(30)]
+print(A)
+
+MAX_CLOSED_TIME = 5
+OPENED = set()
+CACHE = defaultdict(int)
+
+
+def get_cache_key(curr_hour, inside_air, time_left_to_be_closed):
+    """
+    The string formed from current hour, inside air value
+    and time to left to be closed is the combination which uniquely specifies
+    the current algorithm state.
+    """
+    return "%s_%s_%s" % (curr_hour, inside_air, time_left_to_be_closed)
+
+
+def solve(curr_hour, inside_air, time_left_to_be_closed):
+
+    # result is in cache return the result
+    if get_cache_key(curr_hour, inside_air, time_left_to_be_closed) in CACHE:
+        return CACHE[
+            get_cache_key(curr_hour, inside_air, time_left_to_be_closed)]
+
+    outside_air = A[curr_hour]
+
+    # open open, there is no time to left
+    if time_left_to_be_closed == 1:
+        inside_air = outside_air
+
+    # we reached the end of the times in our calculations
+    if curr_hour == len(A) - 1:
+        return inside_air
+
+    # assume we should open right now
+    open_now = outside_air + solve(
+        curr_hour + 1,
+        outside_air,
+        MAX_CLOSED_TIME)
+
+    # assume we should stay closed
+    stay_closed = inside_air + solve(
+        curr_hour + 1,
+        inside_air,
+        time_left_to_be_closed - 1)
+
+    # return the min value we get from our assumptions
+    if open_now < stay_closed:
+        OPENED.add(curr_hour)
+        min_res = open_now
+    else:
+        if curr_hour in OPENED:
+            OPENED.remove(curr_hour)
+        min_res = stay_closed
+
+    # cache the result
+    CACHE[
+        get_cache_key(curr_hour, inside_air, time_left_to_be_closed)] = min_res
+    return min_res
+
+
+print(solve(0, A[0], MAX_CLOSED_TIME))
+print(OPENED)
--- stupidsort.py
+++ stupidsort.py
@@ -1,30 +1,40 @@
+import time
 import random
 from copy import copy
-import time
 
-n = 10
+n = 1000
 arr = [random.randint(0, n) for i in range(n)]
-c = 0
 
-def checksorted(arr):
-    last = -1
-    for i, a in enumerate(arr):
+
+def is_sorted(arr):
+    last = arr[0]
+    for a in arr:
         if a < last:
             return False
         last = a
     return True
 
 
+def shuffle(arr):
+    l = 0
+    r = len(arr) - 1
+    while l < r:
+        m = random.randint(l, r)
+        if arr[l] <= arr[m]:
+            l += 1
+            continue
+        arr[m], arr[l] = arr[l], arr[m]
+        l += 1
+    return arr
+
 def stupid_sort(arr):
     while True:
-        if c % 1000 == 0:
-            print(arr)
-        random.shuffle(arr)
-        if checksorted(arr):
+        shuffle(arr)
+        print(arr)
+        if is_sorted(arr):
             return arr
 
 
 st = time.time()
-res = stupid_sort(copy(arr))
+print(stupid_sort(copy(arr)))
 print(time.time() - st)
-print(res)
