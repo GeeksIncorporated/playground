eb4f0f013ed62f9baffa6a49a691fd7b86edb4be
aliowka <aliowka@gmail.com>
2018-03-13 01:08:34 +0200

    Added: more problems

--- mazgan.py
+++ mazgan.py
@@ -1,16 +1,7 @@
 import sys
-import random
 from collections import defaultdict
 
 sys.setrecursionlimit(2024)
-
-# creating random array
-A = [random.randint(0, 100) for i in range(30)]
-print(A)
-
-MAX_CLOSED_TIME = 5
-OPENED = set()
-CACHE = defaultdict(int)
 
 
 def get_cache_key(curr_hour, inside_air, time_left_to_be_closed):
@@ -22,49 +13,88 @@ def get_cache_key(curr_hour, inside_air, time_left_to_be_closed):
     return "%s_%s_%s" % (curr_hour, inside_air, time_left_to_be_closed)
 
 
-def solve(curr_hour, inside_air, time_left_to_be_closed):
+def mazgan(A, time_left_till_forced_opened):
+    cache = defaultdict(int)
+    opened = defaultdict(int)
+    max_closed_time = time_left_till_forced_opened
+    print "In:", time_left_till_forced_opened, A
+    if time_left_till_forced_opened == 1:
+        return (sum(A), A)
 
-    # result is in cache return the result
-    if get_cache_key(curr_hour, inside_air, time_left_to_be_closed) in CACHE:
-        return CACHE[
-            get_cache_key(curr_hour, inside_air, time_left_to_be_closed)]
+    def solve(curr_hour, inside_air, time_left_to_be_closed):
+        # we reached the end of the times in our calculations
+        if curr_hour == len(A):
+            return 0
 
-    outside_air = A[curr_hour]
+        key = get_cache_key(curr_hour, inside_air, time_left_to_be_closed)
 
-    # open open, there is no time to left
-    if time_left_to_be_closed == 1:
-        inside_air = outside_air
+        # result is in cache return the result
+        if key in cache:
+            return cache[key]
 
-    # we reached the end of the times in our calculations
-    if curr_hour == len(A) - 1:
-        return inside_air
+        outside_air = A[curr_hour]
 
-    # assume we should open right now
-    open_now = outside_air + solve(
-        curr_hour + 1,
-        outside_air,
-        MAX_CLOSED_TIME)
+        # open open, there is no time to left
+        if time_left_to_be_closed == 1:
+            return outside_air + solve(
+                curr_hour + 1,
+                outside_air,
+                max_closed_time)
 
-    # assume we should stay closed
-    stay_closed = inside_air + solve(
-        curr_hour + 1,
-        inside_air,
-        time_left_to_be_closed - 1)
+        # assume we should open right now
+        open_now = outside_air + solve(
+            curr_hour + 1,
+            outside_air,
+            max_closed_time)
 
-    # return the min value we get from our assumptions
-    if open_now < stay_closed:
-        OPENED.add(curr_hour)
-        min_res = open_now
-    else:
-        if curr_hour in OPENED:
-            OPENED.remove(curr_hour)
-        min_res = stay_closed
+        # assume we should stay closed
+        stay_closed = inside_air + solve(
+            curr_hour + 1,
+            inside_air,
+            time_left_to_be_closed - 1)
 
-    # cache the result
-    CACHE[
-        get_cache_key(curr_hour, inside_air, time_left_to_be_closed)] = min_res
-    return min_res
+        # return the min value we get from our assumptions
+        if open_now <= stay_closed:
+            opened[curr_hour] += 1
+            min_res = open_now
+        else:
+            opened[curr_hour] -= 1
+            min_res = stay_closed
+
+        # cache the result
+        cache[key] = min_res
+        return min_res
+
+    integral = (solve(0, A[0], max_closed_time))
+
+    # filter the indeces dict by positive values (those hours when was opened
+    # more times than closed)
+    indices_when_opened_more_times_than_closed = [
+        k for k, v in opened.items() if v > 0]
+    return integral, indices_when_opened_more_times_than_closed
 
 
-print(solve(0, A[0], MAX_CLOSED_TIME))
-print(OPENED)
+A = [1, 2, 3, 4, 5]
+assert mazgan(A, 10) == (5, [0])
+
+A = [1, 5, 5, 5]
+assert mazgan(A, 3) == (8, [0, 3])
+
+A = [1, 5, 0, 5]
+assert mazgan(A, 3) == (2, [0, 2])
+
+A = [1, 2, 3, 4, 5, 0, 1]
+assert mazgan(A, 3) == (11, [0, 2, 5])
+
+A = [1, 5, 1, 5]
+assert mazgan(A, 1) == (12, [1, 5, 1, 5])
+
+A = [3, 2, 1, 0, 9, 8, 7]
+assert mazgan(A, 3) == (13, [0, 1, 2, 3, 6])
+
+# creating random array
+import random
+
+A = [random.randint(0, 10) for i in range(20)]
+time_left_till_forced_opened = random.randint(1, len(A) / 2)
+print(mazgan(A, time_left_till_forced_opened))
